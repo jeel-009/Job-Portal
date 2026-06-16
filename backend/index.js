c76621f10debfd0682cdd5c9e8d9a7cb9044a987
jeel malani <jeelmalani12@gmail.com>
2026-06-16 18:52:35 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import connectDB from '../utils/db.js'
+import connectDB from './utils/db.js'
 
 dotenv.config()
 
-import userRoute from '../routes/user.route.js'
-import companyRoute from '../routes/company.route.js'
-import jobRoute from '../routes/job.route.js'
-import applicationRoute from '../routes/application.route.js'
+import userRoute from './routes/user.route.js'
+import companyRoute from './routes/company.route.js'
+import jobRoute from './routes/job.route.js'
+import applicationRoute from './routes/application.route.js'
 
 const app = express()
 
@@ -22,6 +22,12 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Backend is working"
+    })
+})
 
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/company', companyRoute)
