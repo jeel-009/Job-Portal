d98af512f2096a11ca7309764dd1ac659f4ce3d1
jeel malani <jeelmalani12@gmail.com>
2026-06-16 20:23:06 +0530

    update

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,4 +1,7 @@
-export const USER_API_END_POINT = 'https://job-portal-backend.vercel.app/api/v1/user'
-export const JOBS_API_END_POINT = 'https://job-portal-backend.vercel.app/api/v1/job'
-export const APPLICATION_API_END_POINT = 'https://job-portal-backend.vercel.app/api/v1/application'
-export const COMPANY_API_END_POINT = 'https://job-portal-backend.vercel.app/api/v1/company'
+export const USER_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/user`;
+
+export const JOBS_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/job`;
+
+export const APPLICATION_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/application`;
+
+export const COMPANY_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/company`;
