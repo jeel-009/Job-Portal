b66e87e21dcfafe5870b693d64323bdc9d4ca118
jeel malani <jeelmalani12@gmail.com>
2026-06-16 18:43:15 +0530

    update build script

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,18 +2,19 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import connectDB from './utils/db.js'
-dotenv.config({})
+import connectDB from '../utils/db.js'
 
-import userRoute from './routes/user.route.js'
-import companyRoute from './routes/company.route.js'
-import jobRoute from './routes/job.route.js'
-import applicationRoute from './routes/application.route.js'
+dotenv.config()
+
+import userRoute from '../routes/user.route.js'
+import companyRoute from '../routes/company.route.js'
+import jobRoute from '../routes/job.route.js'
+import applicationRoute from '../routes/application.route.js'
 
 const app = express()
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://job-portal-ten-fawn.vercel.app',
     credentials: true
 }
 
@@ -27,13 +28,6 @@ app.use('/api/v1/company', companyRoute)
 app.use('/api/v1/job', jobRoute)
 app.use('/api/v1/application', applicationRoute)
 
+connectDB()
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    connectDB()
-    console.log(`SERVER IS RUNNING AT ${PORT}`)
-})
-
-
-
+export default app
