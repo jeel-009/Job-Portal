f132d44d73354a45e292211e13daccc0ece7e558
jeel malani <jeelmalani12@gmail.com>
2026-06-16 22:02:09 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './utils/db.js'
-
-dotenv.config()
+dotenv.config({})
 
 import userRoute from './routes/user.route.js'
 import companyRoute from './routes/company.route.js'
@@ -14,7 +13,7 @@ import applicationRoute from './routes/application.route.js'
 const app = express()
 
 const corsOptions = {
-    origin: "https://job-portal-yxfm.vercel.app",
+    origin: 'http://localhost:5173',
     credentials: true
 }
 
@@ -23,17 +22,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Backend is working"
-    })
-})
-
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/company', companyRoute)
 app.use('/api/v1/job', jobRoute)
 app.use('/api/v1/application', applicationRoute)
 
-connectDB()
 
-export default app
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`SERVER IS RUNNING AT ${PORT}`)
+})
+
+
+
